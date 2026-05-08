da66ed825c6571f3dd8a39a21131402b0900afd9
Pooja-kanwar006 <poojakanwarbhati24@gmail.com>
2026-05-08 16:10:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import express from "express";
+import express, { request, response } from "express"
 const app=express();
-const port=8888;
+const port=3000;
+
+app.get("/",(request,response)=>{
+response.send("<h1>ji</h1>");
+})
+app.get("/contact",(req,res)=>{
+res.send("<p>my contact:222222</p>");
+})
+app.get("/about",(req,re)=>{
+re.send("<p>my name is pooja</p>");
+})
 app.listen(port,()=>{
-console.log(`server is running on port ${port}`);
+console.log(`servder live at port:${port}`)
 })
